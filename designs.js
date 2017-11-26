3fd3669283fdd0f0ff701702daa2dda40c8ba77b
KamilKonopka <kamil.konopka@thomsonreuters.com>
2017-11-26 16:30:25 +0100

    final version

--- designs.js
+++ designs.js
@@ -1,18 +1,32 @@
-// Select color input
-// Select size input
 
-// When size is submitted by the user, call makeGrid()
 
-function makeGrid() {
-    var rows, columns, tableRows, tableColumns;
-    rows = $('#input_width').val();
-    columns = $('#input_height').val();
-    for (i = 0; i < rows; i++){
-        for (y = 0; y < columns; y++){
-            $('#pixel_canvas').appendChild('<td>' + columns[i] + '</td>');
+
+
+var table, form;
+table = $('#pixel_canvas');
+form = $("#sizePicker");
+
+function makeGrid(){
+    var table;
+    table = $('#pixel_canvas');
+    table.empty();//clears previous table if existing
+    const columnVal = $('#input_height').val();
+    const rowVal = $('#input_width').val();
+    for (x = 0; x < columnVal; x++){
+        table.append('<tr></tr>');
+        for (y = 0; y < rowVal; y++){
+            table.children().last().append('<td></td>');
         }
-        $('#pixel_canvas').prependChild('<tr>' + rows[i] + '</tr>');
     }
-
 }
-$('#button').on('click', makeGrid());
+
+    table.on('click','td',function(){
+    var cellColor;
+    cellColor = $('#colorPicker').val();
+    $(this).attr('bgcolor', cellColor);
+});
+
+    form.submit(function(event) {
+    event.preventDefault();
+    makeGrid();
+});
